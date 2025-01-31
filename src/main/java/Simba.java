b464aaf0e7343a1b6780f6e72b567182a65c24f5
AisyahAlly <ally@r-105-98-25-172.comp.nus.edu.sg>
2025-01-31 15:33:28 +0800

    add list

--- src/main/java/Simba.java
+++ src/main/java/Simba.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Simba {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";*/
         System.out.println("Hello I am Simba.");
         System.out.println("How can I help you?");
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
         while (true) {
             String command = sc.nextLine();
             if (command.equals("bye")) {
                 System.out.println("Bye-bye!");
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i + ": " + list.get(i));
+                }
             } else {
-                System.out.println(command);
+                System.out.println("added: " + command);
+                list.add(command);
             }
         }
     }
