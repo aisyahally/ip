32fa286cf337a0bfd6182a588c7a9e97b5481bd7
AisyahAlly <ally@r-105-98-25-172.comp.nus.edu.sg>
2025-01-31 15:46:33 +0800

    add mark as done

--- src/main/java/Simba.java
+++ src/main/java/Simba.java
@@ -6,7 +6,7 @@ public class Simba {
         System.out.println("Hello I am Simba.");
         System.out.println("How can I help you?");
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Task> list = new ArrayList<Task>();
         while (true) {
             String command = sc.nextLine();
             if (command.equals("bye")) {
@@ -14,12 +14,50 @@ public class Simba {
                 break;
             } else if (command.equals("list")) {
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println(i + ": " + list.get(i));
+                    int idx = i + 1;
+                    System.out.println(idx + ": " + list.get(i));
+                }
+            } else if (command.contains("mark")) {
+                int i = Integer.parseInt(command.substring(command.length() - 1));
+                if (command.contains("unmark")) {
+                    list.get(i-1).makeUndone();
+                    System.out.println("Okay! This task is not done:");
+                    System.out.println(list.get(i-1));
+                } else {
+                    list.get(i-1).makeDone();
+                    System.out.println("Alright! This task is done:");
+                    System.out.println(list.get(i-1));
                 }
             } else {
                 System.out.println("added: " + command);
-                list.add(command);
+                list.add(new Task(command));
             }
+        }
+    }
+}
+
+class Task {
+    private boolean isDone;
+    private String taskName;
+
+    Task(String name) {
+        this.isDone = false;
+        this.taskName = name;
+    }
+
+    void makeDone() {
+        this.isDone = true;
+    }
+
+    void makeUndone() {
+        this.isDone = false;
+    }
+
+    public String toString() {
+        if (this.isDone) {
+            return "[X] " + this.taskName;
+        } else {
+            return "[ ] " + this.taskName;
         }
     }
 }
