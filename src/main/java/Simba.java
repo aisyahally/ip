e707437b2f10af74587520714c977c007e68da6d
AisyahAlly <ally@r-105-98-25-172.comp.nus.edu.sg>
2025-01-31 15:22:34 +0800

    add greeting

---
+++ src/main/java/Simba.java
@@ -0,0 +1,11 @@
+public class Simba {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        System.out.println("Hello I am Simba.");
+        System.out.println("How can I help you?");
+    }
+}
