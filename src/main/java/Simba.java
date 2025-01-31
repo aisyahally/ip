75aac94d041e92da40d559f7a3e17810ab9a979b
AisyahAlly <ally@r-105-98-25-172.comp.nus.edu.sg>
2025-01-31 15:28:26 +0800

    add echo

--- src/main/java/Simba.java
+++ src/main/java/Simba.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Simba {
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -7,5 +9,15 @@ public class Simba {
                 + "|____/ \\__,_|_|\\_\\___|\n";*/
         System.out.println("Hello I am Simba.");
         System.out.println("How can I help you?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye-bye!");
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 }
