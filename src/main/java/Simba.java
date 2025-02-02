d2e168cf7c3bee0b47498831851a637e140ada3a
aisyah ally <>
2025-02-02 16:19:02 +0800

    Reapply "Save tasks in a separate file"
    This reverts commit 0f1bc9b71e02e55382cdfc402195bdd75a28e3d9.

--- src/main/java/Simba.java
+++ src/main/java/Simba.java
@@ -1,23 +1,59 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Simba {
+    private static void printFile(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
+        Scanner sc = new Scanner(file);
+        if (!sc.hasNext()) {
+            System.out.println("\tTask list is empty");
+        }
+        while (sc.hasNext()) {
+            System.out.println("\t" + sc.nextLine());
+        }
+    }
+
+    private static void writeToFile(String filePath, ArrayList<Task> list) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        for (int i = 0; i < list.size(); i++) {
+            int idx = i + 1;
+            fw.write(idx + ": " + list.get(i) + "\n");
+        }
+        fw.close();
+    }
+
     public static void main(String[] args) {
         try {
+            String filePath = "simba.txt";
+            File file = new File(filePath);
             System.out.println("\tHello I am Simba :D");
             System.out.println("\tHow can I help you?");
             Scanner sc = new Scanner(System.in);
             ArrayList<Task> list = new ArrayList<Task>();
             while (true) {
                 String command = sc.nextLine();
-                if (command.equals("bye")) {
+                writeToFile(filePath, list);
+                if (command.equals("hello")) {
+                    System.out.println("\tHello! What would you like me to do today?");
+                } else if (command.equals("help")) {
+                    System.out.println("\t Here are the list of commands:");
+                    System.out.println("\t \t - hello");
+                    System.out.println("\t \t - list");
+                    System.out.println("\t \t - todo [task description]");
+                    System.out.println("\t \t - deadline [task description] /by [date and/or time]");
+                    System.out.println("\t \t - event [task description] /by [date and/or time] /to [date and/or time]");
+                    System.out.println("\t \t - mark [task number] / unmark [task number]");
+                    System.out.println("\t \t - delete [task number]");
+                    System.out.println("\t \t - bye");
+                } else if (command.equals("bye")) {
                     System.out.println("\tBye-bye!");
                     break;
                 } else if (command.equals("list")) {
-                    for (int i = 0; i < list.size(); i++) {
-                        int idx = i + 1;
-                        System.out.println(idx + ": " + list.get(i));
-                    }
+                    printFile(filePath);
                 } else if (command.contains("mark")) {
                     int i = Integer.parseInt(command.substring(command.length() - 1));
                     if (command.contains("unmark")) {
@@ -30,9 +66,9 @@ public class Simba {
                 } else if (command.contains("delete")) {
                     int idx = Integer.parseInt(command.substring(command.length() - 1));
                     System.out.println("\tDeleted task: " + list.get(idx-1));
-                    System.out.println("\tNow you have " + list.size() + " task(s) in the list");
                     list.remove(idx-1);
-                } else {
+                    System.out.println("\tNow you have " + list.size() + " task(s) in the list");
+                } else { //Add a task
                     if (command.contains("todo")) {
                         if (command.length() < 5) {
                             throw new EmptyException("ToDo");
@@ -75,7 +111,6 @@ public class Simba {
                     } else {
                         throw new NonsenseException();
                     }
-
                     System.out.println("\tAdded: " + list.get(list.size() - 1));
                     System.out.println("\tNow you have " + list.size() + " task(s) in the list");
                 }
@@ -84,6 +119,10 @@ public class Simba {
             System.out.println("\tOh no! " + e.getMessage() + " description is wrong");
         } catch (NonsenseException e) {
             System.out.println("\tOh dear :( I don't understand you");
+        } catch (IOException e) {
+            System.out.println("\t Something went wrong with the file: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("\t Please refer to the proper command formats");
         }
     }
 }
